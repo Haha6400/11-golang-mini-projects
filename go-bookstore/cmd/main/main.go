b23516bb30c3f73a4b9487d686a6a028eaf6c967
Haha06 <21020310@vnu.edu.vn>
2023-10-26 20:53:02 +0700

    note: cant get package

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Haha6400/11-golang-mini-projects/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
