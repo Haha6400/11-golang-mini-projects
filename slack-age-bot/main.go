a8ba8e820ff8ca6cf09436bdd935b194c735596f
Haha06 <21020310@vnu.edu.vn>
2023-11-04 22:58:25 +0700

    create a slack age bot

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/shomali11/slacker"
 )
 
+// Use analyticChannel to receive commans events from the Slack bot.
 func printCommandEvent(analyticChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticChannel {
 		fmt.Println("Command Events")
@@ -21,11 +23,26 @@ func printCommandEvent(analyticChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6101182274725-6125533944708-bn3Tw255YZROGrhxMlmVewlo")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A06323ET118-6116761573425-1cf2b081e0f06046f59ef734e85412ed1c084001ec2d669cd0f31f9c91c55bbe")
+	// Set environment variables
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6101182274725-6142744918517-IQSEfEO5Mt4BXnKmpBfI2Msv")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A063V3HA1HD-6139013502054-34e0e9cb687dc81173a58b851cda7826addd09019827e57c044d3f5c32f09ec1")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 	go printCommandEvent(bot.CommandEvents())
+	bot.Command("my year of birth is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				response.Reply("Error")
+				return
+			}
+			age := 2023 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
